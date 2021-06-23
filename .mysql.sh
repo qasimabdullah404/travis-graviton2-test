3aaa35110ba70b0f8a674c4dcbfce36c4ec463dd
Qasim Abdullah <qasimabdullah404@gmail.com>
2021-06-23 23:55:25 +0500

    TEST

--- .mysql.sh
+++ .mysql.sh
@@ -3,4 +3,5 @@ wget https://dev.mysql.com/get/mysql-apt-config_0.7.3-1_all.deb
 sudo dpkg --install mysql-apt-config_0.7.3-1_all.deb
 sudo apt-get update -q
 sudo apt-get install -q -y --allow-unauthenticated -o Dpkg::Options::=--force-confnew mysql-server
+sudo systemctl restart mysql
 sudo mysql -u root -e "CREATE DATABASE drupal; CREATE USER 'drupal'@'localhost' IDENTIFIED BY 'drupal'; GRANT ALL ON drupal.* TO 'drupal'@'localhost';"
